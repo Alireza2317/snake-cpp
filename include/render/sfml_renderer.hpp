58b833efd56d3e0f78d1826022993de187d52de9
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-30 03:12:49 +0200

    defined SFMLRenderer header

--- include/render/sfml_renderer.hpp
+++ include/render/sfml_renderer.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include "common/types.hpp"
+#include "interfaces/renderer.hpp"
+#include <SFML/Graphics.hpp>
+
+class SFMLRenderer : public IRenderer {
+  private:
+	sf::RenderWindow m_window;
+  public:
+	SFMLRenderer(uint16_t width, uint16_t height, const std::string& title);
+
+	virtual void clear() override;
+	virtual void draw_square(snake::common::Position pos, snake::common::Color color) override;
+	virtual void display() override;
+
+	virtual bool is_open() const override;
+	virtual void handle_events() override;
+};
