ba90908fa9396ad5bae8cca59a23020c7357d76d
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-30 02:25:34 +0200

    added a basic Snake class

--- include/core/snake.hpp
+++ include/core/snake.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include "common/types.hpp"
+#include <vector>
+
+class Snake {
+	using Direction = snake::common::Direction;
+	using Position = snake::common::Position;
+
+  private:
+	Position left_over_tail;
+	void move_head();
+
+  public:
+	std::vector<Position> parts;
+	Direction direction{Direction::Right};
+
+	void move();
+	void turn(Direction new_direction);
+};
---
+++ src/core/snake.cpp
@@ -0,0 +1,36 @@
+#include "core/snake.hpp"
+
+void Snake::move_head() {
+	Position head = parts.at(0);
+	switch (direction) {
+		case Direction::Left:
+			head.x--;
+			break;
+		case Direction::Up:
+			head.y--;
+			break;
+		case Direction::Right:
+			head.x++;
+			break;
+		case Direction::Down:
+			head.y++;
+			break;
+	}
+}
+
+void Snake::move() {
+	move_head();
+
+	left_over_tail = parts.back();
+	parts.pop_back();
+}
+
+void Snake::turn(Direction new_direction) {
+	static auto is_horizontal = [](Direction d) constexpr {
+		return d == Direction::Right or d == Direction::Left;
+	};
+
+	if (is_horizontal(new_direction) != is_horizontal(direction)) {
+		direction = new_direction;
+	}
+}
