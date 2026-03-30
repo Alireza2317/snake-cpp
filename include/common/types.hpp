83489089b4cd9020652dff78dc9a65366cf2bc02
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-30 02:24:59 +0200

    added common types header

--- include/common/types.hpp
+++ include/common/types.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include <cstdint>
+
+namespace snake::common {
+	struct Position {
+		int16_t x, y;
+
+		bool operator==(const Position& other) const = default;
+
+		bool is_valid() {
+			return not(x < 0 or y < 0);
+		}
+	};
+
+	enum class Direction { Left, Up, Right, Down };
+} // namespace snake::common
