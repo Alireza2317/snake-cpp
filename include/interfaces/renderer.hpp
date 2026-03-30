6c056585c630e2773959bf39f6c7708b903696af
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-30 03:02:25 +0200

    defined renderer interface

--- include/interfaces/renderer.hpp
+++ include/interfaces/renderer.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+class IRenderer {
+  public:
+	virtual ~IRenderer() = default;
+
+	virtual void clear() = 0;
+	virtual void draw_square() = 0;
+	virtual void display() = 0;
+
+	virtual bool is_open() const = 0;
+	virtual void handle_events() = 0;
+};
