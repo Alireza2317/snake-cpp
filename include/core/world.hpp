97fff8651f24e2638deda42d0b4386c254afc3ec
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-31 00:41:50 +0200

    implemented World methods

--- include/core/world.hpp
+++ include/core/world.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "common/types.hpp"
-#include "interfaces/renderer.hpp"
 #include <cstdint>
 #include <random>
 #include <vector>
@@ -13,11 +12,12 @@ class World {
 	uint16_t m_height;
 	std::vector<Position> m_obstacles;
 	Position m_food;
-	std::mt19937 m_rng;
+	std::mt19937 m_rng = std::mt19937{std::random_device{}()};
 
   public:
 	World(uint16_t width, uint16_t height, std::vector<Position> obstacles);
+	static bool is_member(const Position& pos, std::vector<Position> positions);
+	bool is_occupied(Position pos, const std::vector<Position>& exclude_positions = {});
 	bool is_in_bounds(Position pos);
 	void spawn_food(const std::vector<Position>& exclude_positions);
-	void render(IRenderer& renderer);
 };
--- src/core/world.cpp
+++ src/core/world.cpp
@@ -2,9 +2,34 @@
 #include <algorithm>
 #include <cstdint>
 #include <print>
+#include <random>
 #include <vector>
 
 World::World(uint16_t width, uint16_t height, std::vector<Position> obstacles)
 	: m_width(width), m_height(height), m_obstacles(std::move(obstacles)) {
 }
 
+bool World::is_in_bounds(Position pos) {
+	return pos.x >= 0 and pos.y >= 0 and pos.x < m_width and pos.y < m_height;
+}
+
+bool World::is_member(const Position& pos, std::vector<Position> positions) {
+	return std::find(positions.begin(), positions.end(), pos) != positions.end();
+}
+
+bool World::is_occupied(Position pos, const std::vector<Position>& exclude_positions) {
+	if (not is_in_bounds(pos)) {
+		std::println("Position ({}, {}) is out of bounds!", pos.x, pos.y);
+		return true;
+	}
+	return is_member(pos, m_obstacles) or is_member(pos, exclude_positions);
+}
+
+void World::spawn_food(const std::vector<Position>& exclude_positions) {
+	do {
+		std::uniform_int_distribution<int16_t> rnd_x(0, m_width - 1);
+		std::uniform_int_distribution<int16_t> rnd_y(0, m_width - 1);
+		m_food = {rnd_x(m_rng), rnd_y(m_rng)};
+
+	} while (is_occupied(m_food, exclude_positions));
+}
