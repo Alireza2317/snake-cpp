ce0a9024ee96c84ea9ecef6cc0862da78d0c6696
Alireza Alavi <alireza.alavi237@gmail.com>
2026-03-31 03:07:09 +0200

    added some useful enums to common types

--- include/common/types.hpp
+++ include/common/types.hpp
@@ -12,7 +12,23 @@ namespace snake::common {
 		}
 	};
 
-	enum class Color { White, Black, Gray, Yellow, Orange, Green, Blue, Red, Violet, Cyan, Transparent };
+	enum class Color {
+		White,
+		Black,
+		Gray,
+		Yellow,
+		Orange,
+		Green,
+		Blue,
+		Red,
+		Violet,
+		Cyan,
+		Transparent
+	};
 
 	enum class Direction { Left, Up, Right, Down };
+
+	enum class GameState { Running, Paused, Won, HitWall, HitSelf, Quit };
+
+	enum class Command { None, MoveLeft, MoveUP, MoveRight, MoveDown, Pause, Quit };
 } // namespace snake::common
