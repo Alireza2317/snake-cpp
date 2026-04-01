eefe373cb55c7e5e317758c63241f0eaf0154994
Alireza Alavi <alireza.alavi237@gmail.com>
2026-04-01 20:02:36 +0200

    finished off App

--- include/app/application.hpp
+++ include/app/application.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "common/config.hpp"
+#include "common/types.hpp"
 #include "core/game.hpp"
 #include "interfaces/renderer.hpp"
 #include "render/sfml_renderer.hpp"
@@ -27,10 +28,27 @@ inline App::App()
 }
 
 inline void App::run() {
-	while (m_renderer->is_open()) {
-		m_renderer->handle_events();
+	sf::Clock clock;
+	sf::Time time = sf::Time::Zero;
 
-		m_game.step();
+	const sf::Time time_per_step = sf::seconds(snake::config::game::DELAY);
+
+	while (m_renderer->is_open()) {
+		time += clock.restart();
+
+		snake::common::Command cmd = m_renderer->poll_command();
+
+		if (cmd == snake::common::Command::Quit) {
+			m_renderer->close();
+			break;
+		}
+
+		m_game.handle_command(cmd);
+
+		while (time >= time_per_step) {
+			time -= time_per_step;
+			m_game.step();
+		}
 
 		m_renderer->clear();
 		m_game.render(*m_renderer);
